7f2453ce946f7ee9d41bfac5d240afed0a315125
Amarnath K <itsrook2005@gmail.com>
2025-07-23 00:51:23 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -4,13 +4,13 @@ int main() {
     printf("Enter the limit\t: ");
     scanf("%d", &limit);
     if (limit < 1){
-        printf("Please enter a postive natural number");
+        printf("Please enter a positive natural number\n");
         return 1;
     }
     while (i <= limit) {
         sum += i;
         i++;
     }
-    printf("Sum of first %d natuaral numbers is %d\n", limit, sum);
+    printf("Sum of first %d natural numbers is %d\n", limit, sum);
     return 0;
 }
