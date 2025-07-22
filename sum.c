a57b8806943ce3612fbf8678ebb1a614060961bc
Amarnath K <itsrook2005@gmail.com>
2025-07-23 00:47:55 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int limit, sum = 0, i = 1;
+    printf("Enter the limit\t: ");
+    scanf("%d", &limit);
+    while (i <= limit) {
+        sum += i;
+        i++;
+    }
+    printf("Sum of first %d natuaral numbers is %d\n", limit, sum);
+    return 0;
+}
