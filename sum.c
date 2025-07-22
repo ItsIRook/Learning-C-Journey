5d3d97eb4cc447e09b7ee6f7dc96a0abecb51c25
Amarnath K <itsrook2005@gmail.com>
2025-07-23 00:50:31 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -3,6 +3,10 @@ int main() {
     int limit, sum = 0, i = 1;
     printf("Enter the limit\t: ");
     scanf("%d", &limit);
+    if (limit < 1){
+        printf("Please enter a postive natural number");
+        return 1;
+    }
     while (i <= limit) {
         sum += i;
         i++;
